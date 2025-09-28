9ee45a5febe6e910d986e2ecf7d52a984b35ecc4
4bd3lb4554t <abdoqubai@gmail.com>
2025-09-28 09:45:09 +0100

    niiice site

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import Projects from './components/Projects';
 import Skills from './components/Skills';
 import Contact from './components/Contact';
 
+
 const App: React.FC = () => {
   return (
     <div className="min-h-screen bg-primary text-white">
